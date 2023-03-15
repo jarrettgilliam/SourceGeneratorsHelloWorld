c466e5eb2a73bd7d6075dca7a0968056c4f0a69c
Jarrett Gilliam <jarrettgilliam@gmail.com>
2023-03-15 12:05:43 -0500

    Source generators work

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/source-generators-overview
+
+namespace ConsoleApp;
+
+public static partial class Program
+{
+    private static void Main()
+    {
+        HelloFrom("Generated Code");
+    }
+}
